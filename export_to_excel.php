4c4543ec17c356642d85e811fe600974b98183ed
Accentel <shiva.accentel.com>
2024-11-08 14:54:37 +0530

    second commit

--- export_to_excel.php
+++ export_to_excel.php
@@ -3,13 +3,13 @@ require_once "connection.php";
 //header("Content-Type: application/vnd.ms-excel");
 //header("Content-disposition: attachment; filename=marks_report_printexcel".date('d-m-Y').".xls");
 
-header('Content-type: application/vnd.ms-excel');
+ header('Content-type: application/vnd.ms-excel'); 
 
 // It will be called file.xls
-header("Content-Disposition: attachment; filename=Products-download" . date('d-m-Y') . ".xls");
+ header("Content-Disposition: attachment; filename=Products-download" . date('d-m-Y') . ".xls"); 
 
-header('Cache-Control: max-age=0');
-//$objWriter->save('php://output');
+ header('Cache-Control: max-age=0'); 
+//$objWriter->save('php://output'); 
 
 //echo"<table border='1'><tr><th></th><th width='2%'>Location:</th><th>$val4</th><th >From Date:</th><th>$val5</th><th>Todate:</th><th>$val7</th></tr></table>";
 echo "<table width='100%' border='1'><tr><th style='background-color:blue; color:white'>S NO</th><th style='background-color:blue; color:white' width='10%' >Site name</th><th style='background-color:blue ;color:white' width='10%' >Indus Id</th><th style='background-color:blue ;color:white' width='15%'>Date</th><th style='background-color:blue ;color:white'>Upload By</th></tr>";
